b557b20b3737117852aa5f0481fdadbca1590fc5
Jim Duey <jim@intensivesystems.net>
2010-10-05 17:12:16 -0500

    Fix bug in require namespace

--- src/conduit/require.clj
+++ src/conduit/require.clj
@@ -3,13 +3,13 @@
      arrows.core
      conduit.core))
 
-(with-arrow conduit
-            (def proc a-arr)
-            (def case a-selectp)
-            (def comp a-comp)
-            (def split a-all)
-            (def juxt a-par)
-            (def reduce a-loop)
-            (def map conduit-map)
-            (def do conduit-do))
+(def proc a-arr)
+(def case a-selectp)
+(def comp a-comp)
+(def split a-all)
+(def juxt a-par)
+(def reduce a-loop)
+(def map conduit-map)
+(def do conduit-do)
+            
 
