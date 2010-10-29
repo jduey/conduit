60b697dbac37bf8cc7698c6378ebc69f3b09e618
Tim Dysinger <tim@dysinger.net>
2010-10-29 10:26:55 -0400

    Updated description

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-  :description "conduit - stream processing in Clojure.
-               Similar to Apache Camel"
 (defproject conduit "0.7.0-SNAPSHOT"
+  :description "Conduit: Stream Processing in Clojure."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [arrows "1.2.0"]])
