af5eb8dc7fa8945a53df0c91cdd803a4c08515a0
Tim Dysinger <tim@dysinger.net>
2010-10-29 10:25:29 -0400

    Project name change

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject net.intensivesystems/conduit "0.7.0-SNAPSHOT"
   :description "conduit - stream processing in Clojure.
                Similar to Apache Camel"
+(defproject conduit "0.7.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [net.intensivesystems/arrows "1.2.0"]])
+                 [arrows "1.2.0"]])
