8cdd28c5d2c2b51d40775c14c4e395a31967684c
Joe Gallo <joegallo@gmail.com>
2010-11-09 14:51:13 -0500

    Add lambda repository.

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "Conduit: Stream Processing in Clojure."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [arrows "1.2.0"]])
+                 [arrows "1.2.0"]]
+  :repositories {"lambda" "http://lambda.sa2s.us/snapshots/"})
