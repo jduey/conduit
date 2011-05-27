c37ed71fc7d1d275e999caed5cf7b47bc30ed271
Joe Gallo <joegallo@gmail.com>
2011-05-27 11:36:38 -0400

    update dependencies

--- project.clj
+++ project.clj
@@ -2,13 +2,10 @@
   :description "Conduit: Stream Processing in Clojure."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [arrows "1.2.0"]
-                 [swank-clojure "1.2.1"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]
-                     [org.clojars.technomancy/clj-stacktrace
-                      "0.2.1-20101126.163600-6"]
-                     [lein-difftest "1.3.2-20101010.033133-1"
-                      :exclusions [clj-stacktrace]]
+                 [arrows "1.2.0"]]
+  :dev-dependencies [[clj-stacktrace "0.2.2"]
+                     [lein-difftest "1.3.3"]
+                     [lein-fail-fast "1.0.0"]
                      [lein-release "1.1.1"]
-                     [lein-fail-fast "1.0.0"]]
+                     [swank-clojure "1.2.1"]]
   :repositories {"lambda" "http://lambda.sa2s.us/snapshots/"})
