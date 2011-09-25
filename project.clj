7601bc8db7a1ad5bd896696e328708d76cdb13e1
Jim Duey <jim@intensivesystems.net>
2011-09-25 14:12:18 -0500

    Fixed name of library

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject conduit "0.8.6"
+(defproject net.intensivesystems/conduit "0.8.6"
   :description "Conduit: Stream Processing in Clojure."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
